68a09199046914da3904e4de2ece55674083194e
Jazcash <jaspercashmore@gmail.com>
2023-05-26 18:47:45 +0100

    stuff

--- src/tachyon-server.ts
+++ src/tachyon-server.ts
@@ -1,35 +1,24 @@
 import WebSocket, { Data, Server, ServerOptions } from "ws";
 import type { Tachyon } from "../tachyon/src/schema";
+import { ServiceId, ResponseType, ResponseEndpointId, RequestEndpointId, RequestType } from "../tachyon/src/helpers";
+import { Signal } from "jaz-ts-utils";
 
-import Ajv from "ajv";
+import Ajv, { ValidateFunction } from "ajv";
 import fs from "fs";
 import path from "path";
 
-type ServiceId = keyof Tachyon;
-type EndpointId<S extends ServiceId> = keyof Tachyon[S];
-type ResponseEndpointId<S extends ServiceId> = keyof {
-    [key in keyof Tachyon[S] as Tachyon[S][key] extends { response: any } ? key : never]: Tachyon[S][key];
-};
-type A = ResponseEndpointId<"init">;
-// type ResponseCommands = {
-//     [serviceId in keyof Tachyon]: {
-//         [endpointId in keyof Tachyon[serviceId]]: Tachyon[serviceId][endpointId] extends { response: infer Res } ? Res : never;
-//     }[keyof Tachyon[serviceId]];
-// }[keyof Tachyon];
-type Response<S extends ServiceId, E extends ResponseEndpointId<S>> = Tachyon[S][E] extends { response: infer Res } ? Res : {};
-
 type Handlers = {
-    [serviceId in keyof Tachyon]?: {
-        [endpointId in keyof Tachyon[serviceId] as Tachyon[serviceId][endpointId] extends { response: any } ? endpointId : never]: Tachyon[serviceId][endpointId] extends { response: infer Res }
+    [serviceId in keyof Tachyon as string]?: {
+        [endpointId in keyof Tachyon[serviceId] as Tachyon[serviceId][endpointId] extends { response: any } ? endpointId & string : never]: Tachyon[serviceId][endpointId] extends { response: infer Res }
             ? Tachyon[serviceId][endpointId] extends { request: { data: infer Req } }
-                ? (data: Req) => Res extends { command: string } ? Omit<Res, "command"> : never
-                : () => Res extends { command: string } ? Omit<Res, "command"> : never
+                ? (data: Req) => Res extends { command: string } ? Promise<Omit<Res, "command">> : never
+                : () => Res extends { command: string } ? Promise<Omit<Res, "command">> : never
             : never;
     };
 };
 
 const ajv = new Ajv({ coerceTypes: true });
-const validators: Map<string, Ajv.ValidateFunction> = new Map();
+const validators: Map<string, ValidateFunction> = new Map();
 
 const services = fs.readdirSync("./tachyon/schema");
 for (const serviceId of services.filter((s) => !s.endsWith(".json"))) {
@@ -45,21 +34,22 @@ for (const serviceId of services.filter((s) => !s.endsWith(".json"))) {
     }
 }
 
-const handlers: Handlers = {
-    auth: {
-        register: (data) => {
-            return {
-                status: "success",
-            };
-        },
-        getToken: (data) => {
-            return {
-                status: "failed",
-                reason: "invalid_password",
-            };
-        },
-    },
-};
+const handlers = (() => {
+    return {
+        auth: {
+            register: async (data) => {
+                return {
+                    status: "success"
+                }
+            },
+            getToken: async (data) => {
+                return {
+                    status: "success"
+                }
+            }
+        }
+    }
+}) satisfies () => Handlers;
 
 export class TachyonServer {
     protected wss: Server;
@@ -81,16 +71,15 @@ export class TachyonServer {
 
 class Client {
     protected socket: WebSocket;
+    protected requestSignals: Map<string, Signal> = new Map();
+    protected handlers: Handlers;
 
     constructor(socket: WebSocket) {
         this.socket = socket;
 
-        // this.sendResponse("init", "init", {
-        //     status: "success",
-        //     data: {
-        //         tachyonVersion: "1.0.0",
-        //     },
-        // });
+        this.handlers = handlers();
+
+        socket.on("message", (data) => this.handleRequest(data));
 
         this.sendResponse("init", "init", {
             status: "success",
@@ -98,15 +87,42 @@ class Client {
                 tachyonVersion: "1.2",
             },
         });
-
-        socket.on("message", (data) => this.handleRequest(data));
     }
 
-    protected handleRequest(data: Data) {
-        console.log(data);
+    public on<S extends ServiceId<Tachyon>, E extends RequestEndpointId<Tachyon, S>>(serviceId: S, endpointId: E) : Signal<RequestType<Tachyon, S, E>> {
+        const commandId = `${serviceId}/${endpointId.toString()}/request`;
+        let signal = this.requestSignals.get(commandId);
+        if (!signal) {
+            signal = new Signal();
+            this.requestSignals.set(commandId, signal);
+        }
+
+        return signal;
     }
 
-    protected sendResponse<S extends ServiceId, E extends ResponseEndpointId<S>>(service: S, endpoint: E, data: Omit<Response<S, E>, "command">) {
+    protected async handleRequest(message: Data) {
+        try {
+            const request = JSON.parse(message.toString()) as { command: `${string}/${string}/request`, data?: object }; // TODO: parse this against an AJV validator
+            const signal = this.requestSignals.get(request.command);
+            if (signal) {
+                signal.dispatch(request);
+            }
+    
+            const [ serviceId, endpointId, commandType ] = request.command.split("/");
+            const service = this.handlers[serviceId];
+            if (service) {
+                const handler = service[endpointId as keyof typeof service] as ((data?: any) => Promise<any>) | undefined;
+                if (handler) {
+                    const response = await handler(request.data)
+                    this.sendResponse(serviceId as any, endpointId as any, response as any);
+                }
+            }
+        } catch (err) {
+            console.error(`Error parsing request`, err);
+        }
+    }
+
+    protected sendResponse<S extends ServiceId<Tachyon>, E extends ResponseEndpointId<Tachyon, S>>(service: S, endpoint: E, data: Omit<ResponseType<Tachyon, S, E>, "command">) {
         const validator = validators.get(`${service}/${endpoint.toString()}/response`);
 
         if (!validator) {
@@ -119,7 +135,7 @@ class Client {
         const response = {
             command: commandId,
             ...data,
-        } as Response<S, E>;
+        } as ResponseType<Tachyon, S, E>;
 
         const isValid = validator(response);
         if (!isValid) {
@@ -131,6 +147,12 @@ class Client {
 
         this.socket.send(JSON.stringify(response));
     }
+
+    // protected validateRequest(request: any) : { command: string, data: object } | never {
+    //     return {
+            
+    //     };
+    // }
 }
 
 class User {}
